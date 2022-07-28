652b405e6be8553fdfcd582ce91c392f97840d30
vladyking <gvladykin@gmail.com>
2022-07-28 11:34:35 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Привет, мир!"); //My first code
+        System.out.println("Привет, мир"); //My first code
     }
 }
